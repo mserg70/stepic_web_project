3ee79393dd11cea854712f176fb636575c3408b4
Serhii Matchenko <serhii.matchenko@gmail.com>
2017-05-13 21:48:45 +0300

    v.2.0 step 1

--- init.sh
+++ init.sh
@@ -6,7 +6,10 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 #gunicorn.conf
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo gunicorn -b 0.0.0.0:8080 hello:app
 sudo /etc/init.d/gunicorn restart
 echo 'netstat -nlpat'
 
