b1de3566b4aae5a9da2372ed21f20b307b938cab
Serhii Matchenko <serhii.matchenko@gmail.com>
2017-05-14 10:22:36 +0300

    v.2.0. step 4

--- hello.py
+++ hello.py
@@ -1,10 +1,6 @@
 def app(environ, start_response):
     data = environ['QUERY_STRING']
-<<<<<<< HEAD
     d = [bytes('\n'.join(str(data).split("&")), 'ascii')]
-=======
-    d = [bytes('\n'.join(str(data).split("&")),'ascii')]
->>>>>>> e98cc4fa2d862408b1544b5d8f12bc164c81f9de
 
     status = '200 OK'
     response_header = [
